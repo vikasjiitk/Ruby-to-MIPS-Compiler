6800ba9830f7a3877a17ace7e674ac592f5b50a9
Janish Jindal <janishjindal95@gmail.com>
2016-02-03 23:37:54 +0530

    Corrected symbol table

--- asgn2/three.py
+++ asgn2/three.py
@@ -14,6 +14,7 @@ class threeAddCode:
 	operator = ""
 	lineNo = 0
 	leader = False
+	flabel = ""
 
 def Equal(i,words):
 	Instr3AC[i].instrType = "Assignment"
@@ -36,16 +37,20 @@ def Ifgoto(i,words):
 		Instr3AC[i].input1 = words[2]
 	Instr3AC[i].target = words[-1]
 def Function(i,words):
-	instrType = "FunctionCall"
-	Instr3AC[i].input1 = words[2]
+	Instr3AC[i].instrType = "FunctionCall"
+	Instr3AC[i].flabel = words[2]
 def Return(i,words):
-	instrType = "return"
+	Instr3AC[i].instrType = "return"
 	if len(words) == 3:
 		Instr3AC[i].input1 = words[2]
 def Label(i,words):
-	instrType = "label"_
+	Instr3AC[i].instrType = "label"
+	Instr3AC[i].flabel = words[2]
+def Print(i,words):
+	Instr3AC[i].instrType  = "print"
+	Instr3AC[i].input1 = words[2]
 
-leaders = [1]
+leaders = [0]
 for line in f:
 	line = line[:-1]
 	words = line.split(',')
@@ -69,45 +74,45 @@ for line in f:
 		Label(i,words)
 	elif words[1] == 'ret':
 		Return(i,words)
+	elif words[1] == 'print':
+		Print(i,words)
 	i = i+1
 leaders.append(i)
-print leaders
+# print leaders
 leaders = sorted(set(leaders))
 NumInstr = i
-for blockNum in range(len(leaders)):
+# Building blockLength number of symbol tables (one for each program point)
+for blockNum in range(len(leaders)-1):
 	initial = leaders[blockNum]	
 	final = leaders[blockNum+1] - 1
-	n = final - initial + 1
-	symtables = [dict() for x in range(n)]
-	for j in range(initial,final+1):
-		if (Instr3AC[j].input1.isdigit() == False and Instr3AC[j].input1!= ""):
-			if Instr3AC[j].input1 not in symtables[0]:
-				for k in range(n):
-					symtables[k][Instr3AC[j].input1] = [Dead, 0]
-		if (Instr3AC[j].input1.isdigit() == False and Instr3AC[j].input1!= ""):
-			if Instr3AC[j].input2 not in symtables[0]:
-				for k in range(n):
-					symtables[k][Instr3AC[j].input2] = [Dead, 0]
-		if Instr3AC[j].output not in symtables[0]:
-				for k in range(n):
-					symtables[k][Instr3AC[j].output] = [Dead, 0]
-	#Backward scanning
-	scan = n-1
-	for j in range(final, initial -1 , -1):
-		if j!=final:
+	blockLength = final - initial + 1
+	symtables = [dict() for x in range(blockLength)]
+	for blockLine in range(initial,final+1):
+		if (Instr3AC[blockLine].input1.isdigit() == False and Instr3AC[blockLine].input1!= ""):
+			if Instr3AC[blockLine].input1 not in symtables[0]:
+				for k in range(blockLength):
+					symtables[k][Instr3AC[blockLine].input1] = [Dead, 0]
+		if (Instr3AC[blockLine].input2.isdigit() == False and Instr3AC[blockLine].input2!= ""):
+			if Instr3AC[blockLine].input2 not in symtables[0]:
+				for k in range(blockLength):
+					symtables[k][Instr3AC[blockLine].input2] = [Dead, 0]
+		if Instr3AC[blockLine].output not in symtables[0] and Instr3AC[blockLine].output!="":
+				for k in range(blockLength):
+					symtables[k][Instr3AC[blockLine].output] = [Dead, 0]
+	# print 'SYMBOL TABLE: '
+	# print symtables
+	#Backward scanning and filling symbol table for each program point
+	scan = blockLength-1
+	for blockLine in range(final, initial -1 , -1):
+		if blockLine!=final:
 			symtables[scan]=symtables[scan+1].copy()								#copying the previous filled dictionary for further changes 
-		if (Instr3AC[j].input1.isdigit() == False and Instr3AC[j].input1!= ""):
-			symtables[scan][Instr3AC[j].input1] = [Live, j]
-		if (Instr3AC[j].input2.isdigit() == False and Instr3AC[j].input2!= ""):
-			symtables[scan][Instr3AC[j].input2] = [Live, j]
-		if (Instr3AC[j].output!= ""):
-			symtables[scan][Instr3AC[j].output] = [Dead, 0]			
+		if (Instr3AC[blockLine].input1.isdigit() == False and Instr3AC[blockLine].input1!= ""):
+			symtables[scan][Instr3AC[blockLine].input1] = [Live, blockLine]
+		if (Instr3AC[blockLine].input2.isdigit() == False and Instr3AC[blockLine].input2!= ""):
+			symtables[scan][Instr3AC[blockLine].input2] = [Live, blockLine]
+		if (Instr3AC[blockLine].output!= ""):
+			symtables[scan][Instr3AC[blockLine].output] = [Dead, 0]			
 		scan = scan-1
-
-
-print leaders
-print Instr3AC[1].lineNo
-print Instr3AC[1].operator
-print Instr3AC[1].input1
-print Instr3AC[1].input2
-print Instr3AC[1].output
+		print 'symbol table before ' + str(blockLine)
+		print symtables[blockLine-initial]
+#	print symtables
