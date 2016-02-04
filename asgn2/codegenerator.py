7cff50cc315ded60e268323e2eff3570816881fc
Vikas Jain <vikasj@cse.iitk.ac.in>
2016-02-04 19:06:59 +0530

    Adding new code_gen definition and assumption file

--- asgn2/codegenerator.py
+++ asgn2/codegenerator.py
@@ -1,16 +1,15 @@
-NumRegister = 8
-RegDescriptor = ['' for x in range(NumRegister)]
-# print RegDescriptor
-variables = []
-for i in range(NumInstr):
-    variables.append(Instr3AC[i].input1)
-    variables.append(Instr3AC[i].input2)
-    variables.append(Instr3AC[i].output)
-
-variables = set(variables)
+NumRegister = 16
+RegDescriptor = {}
 AddDescriptor = {}
-for i in variables:
-    AddDescriptor[i] = ['memory']
+
+def initializeReg:
+    RegAvail = ['$t0','$t1','$t2','$t3','$t4','$t5','$t6','$t7','$s0','$s1','$s2','$s3','$s4','$s5','$s6','$s7']
+    for i in variables:
+        AddDescriptor[i] = ['memory']
+
+def dumpReg:
+    for reg in RegDescriptor:
+        MIPScode.append('sw '+reg+','+RegDescriptor[reg])
 
 def giveOperator(op):
     if op == '+':
@@ -23,136 +22,178 @@ def giveOperator(op):
         return 'div'
     else:
         return 0
-def getreg(instrNo, symTableNo):
-    if Instr3AC[i].instrType in ['Arithmetic']:
-        y = Instr3AC[i].input1
-        if (('register' in AddDescriptor[y]) and (symtables[symTableNo][y] == DEAD):
-                for Regno in range(NumRegister):
-                    if y == RegDescriptor[Regno]:
-                        return Regno
 
-        for Regno in range(NumRegister):
-            if(RegDescriptor[Regno] == ''):
-                return Regno
+def getreg(Instr, var):
 
-        x = Instr3AC[i].output
-        if( symtables[symTableNo][x] == LIVE):
-            farthestNextUse = 0
-            for Var in symtables[symTableNo]:
-                if (symtables[symTableNo][Var][2] > farthestNextUse):
-                    farthestVar = Var
-                    farthestNextUse = symtables[symTableNo][Var][2]
-            for Regno in range(NumRegister):
-                if farthestVar == RegDescriptor[Regno]:
-                    break
-            print 'move %s t%d' % (farthestVar, Regno)                          # moving contents of register to memory
-            AddDescriptor[farthestVar] = ['memory']
-            RegDescriptor[farthestVar] = ''
-            return Regno
+# def getreg(instrNo, symTableNo):
+#     if Instr3AC[i].instrType in ['Arithmetic']:
+#         y = Instr3AC[i].input1
+#         if (('register' in AddDescriptor[y]) and (symtables[symTableNo][y] == DEAD):
+#                 for Regno in range(NumRegister):
+#                     if y == RegDescriptor[Regno]:
+#                         return Regno
+#
+#         for Regno in range(NumRegister):
+#             if(RegDescriptor[Regno] == ''):
+#                 return Regno
+#
+#         x = Instr3AC[i].output
+#         if( symtables[symTableNo][x] == LIVE):
+#             farthestNextUse = 0
+#             for Var in symtables[symTableNo]:
+#                 if (symtables[symTableNo][Var][2] > farthestNextUse):
+#                     farthestVar = Var
+#                     farthestNextUse = symtables[symTableNo][Var][2]
+#             for Regno in range(NumRegister):
+#                 if farthestVar == RegDescriptor[Regno]:
+#                     break
+#             print 'move %s t%d' % (farthestVar, Regno)                          # moving contents of register to memory
+#             AddDescriptor[farthestVar] = ['memory']
+#             RegDescriptor[farthestVar] = ''
+#             return Regno
+#
+#         return x
+#
+#     if Instr3AC[i].instrType == 'Assignment':                                   ## x = y
+#         y = Instr3AC[i].input1
+#         x = Instr3AC[i].output
+#         if y.isdigit() == False and y != '':
+#             if (('register' in AddDescriptor[y]) and (symtables[symTableNo][y] == DEAD):               ## x =y
+#                 for Regno in range(NumRegister):
+#                     if y == RegDescriptor[Regno]:
+#                         return Regno
+#             for Regno in range(NumRegister):
+#                 if(RegDescriptor[Regno] == ''):
+#                     return Regno
+#             if( symtables[symTableNo][x] == LIVE):
+#                 farthestNextUse = 0
+#                 for Var in symtables[symTableNo]:
+#                     if (symtables[symTableNo][Var][2] > farthestNextUse):
+#                         farthestVar = Var
+#                         farthestNextUse = symtables[symTableNo][Var][2]
+#                 for Regno in range(NumRegister):
+#                     if farthestVar == RegDescriptor[Regno]:
+#                         break
+#                 print 'move %s t%d' % (farthestVar, Regno)                                      # moving contents of register to memory
+#                 AddDescriptor[farthestVar] = ['memory']
+#                 RegDescriptor[farthestVar] = ''
+#                 return Regno
+#         else:                                                                                           ### x =2
+#             if (('register' in AddDescriptor[x]) and (symtables[symTableNo][x] == DEAD):
+#                 for Regno in range(NumRegister):
+#                     if x == RegDescriptor[Regno]:
+#                         return Regno
+#                 for Regno in range(NumRegister):
+#                     if(RegDescriptor[Regno] == ''):
+#                         return Regno
+#
+#                 if( symtables[symTableNo][x] == LIVE):
+#                 farthestNextUse = 0
+#                 for Var in symtables[symTableNo]:
+#                     if (symtables[symTableNo][Var][2] > farthestNextUse):
+#                         farthestVar = Var
+#                         farthestNextUse = symtables[symTableNo][Var][2]
+#                 for Regno in range(NumRegister):
+#                     if farthestVar == RegDescriptor[Regno]:
+#                         break
+#                 print 'move %s t%d' % (farthestVar, Regno)                          # moving contents of register to memory
+#                 AddDescriptor[farthestVar] = ['memory']
+#                 RegDescriptor[farthestVar] = ''
+#                 return Regno
 
-        return x
+def code_gen(initial, final):
+    for Instr in range(initial,final+1):
+        if Instr3AC[Instr].operator == '+':
+            reg1 = getreg(Instr, Instr3AC[Instr].input1)
+            reg2 = getreg(Instr, Instr3AC[Instr].input2)
+            reg3 = getreg(Instr, Instr3AC[Instr].output)
+            MIPScode.append('add '+reg3+','+reg1+','+reg2)
 
-    if Instr3AC[i].instrType == 'Assignment':                                   ## x = y
-        y = Instr3AC[i].input1
-        x = Instr3AC[i].output
-        if y.isdigit() == False and y != '':
-            if (('register' in AddDescriptor[y]) and (symtables[symTableNo][y] == DEAD):               ## x =y
-                for Regno in range(NumRegister):
-                    if y == RegDescriptor[Regno]:
-                        return Regno
-            for Regno in range(NumRegister):
-                if(RegDescriptor[Regno] == ''):
-                    return Regno
-            if( symtables[symTableNo][x] == LIVE):
-                farthestNextUse = 0
-                for Var in symtables[symTableNo]:
-                    if (symtables[symTableNo][Var][2] > farthestNextUse):
-                        farthestVar = Var
-                        farthestNextUse = symtables[symTableNo][Var][2]
-                for Regno in range(NumRegister):
-                    if farthestVar == RegDescriptor[Regno]:
-                        break
-                print 'move %s t%d' % (farthestVar, Regno)                                      # moving contents of register to memory
-                AddDescriptor[farthestVar] = ['memory']
-                RegDescriptor[farthestVar] = ''
-                return Regno
-        else:                                                                                           ### x =2                                             
-            if (('register' in AddDescriptor[x]) and (symtables[symTableNo][x] == DEAD):
-                for Regno in range(NumRegister):
-                    if x == RegDescriptor[Regno]:
-                        return Regno
-                for Regno in range(NumRegister):
-                    if(RegDescriptor[Regno] == ''):
-                        return Regno
+        if Instr3AC[Instr].operator == '-':
+            reg1 = getreg(Instr, Instr3AC[Instr].input1)
+            reg2 = getreg(Instr, Instr3AC[Instr].input2)
+            reg3 = getreg(Instr, Instr3AC[Instr].output)
+            MIPScode.append('sub '+reg3+','+reg1+','+reg2)
 
-                if( symtables[symTableNo][x] == LIVE):
-                farthestNextUse = 0
-                for Var in symtables[symTableNo]:
-                    if (symtables[symTableNo][Var][2] > farthestNextUse):
-                        farthestVar = Var
-                        farthestNextUse = symtables[symTableNo][Var][2]
-                for Regno in range(NumRegister):
-                    if farthestVar == RegDescriptor[Regno]:
-                        break
-                print 'move %s t%d' % (farthestVar, Regno)                          # moving contents of register to memory
-                AddDescriptor[farthestVar] = ['memory']
-                RegDescriptor[farthestVar] = ''
-                return Regno
+        if Instr3AC[Instr].operator == '*:
+            reg1 = getreg(Instr, Instr3AC[Instr].input1)
+            reg2 = getreg(Instr, Instr3AC[Instr].input2)
+            reg3 = getreg(Instr, Instr3AC[Instr].output)
+            MIPScode.append('mult '+reg1+','+reg2)
+            MIPScode.append('mflo '+reg3)
 
+        if Instr3AC[Instr].operator == '/:
+            reg1 = getreg(Instr, Instr3AC[Instr].input1)
+            reg2 = getreg(Instr, Instr3AC[Instr].input2)
+            reg3 = getreg(Instr, Instr3AC[Instr].output)
+            MIPScode.append('div '+reg1+','+reg2)
+            MIPScode.append('mflo '+reg3)
 
+        if Instr3AC[Instr].operator == '%:
+            reg1 = getreg(Instr, Instr3AC[Instr].input1)
+            reg2 = getreg(Instr, Instr3AC[Instr].input2)
+            reg3 = getreg(Instr, Instr3AC[Instr].output)
+            MIPScode.append('div '+reg1+','+reg2)
+            MIPScode.append('mfhi '+reg3)
+
+        if Instr3AC[Instr].operator == '=':
+            if (Instr3AC[Instr].input1.isdigit() == True):
+                reg1 = getreg(Instr, Instr3AC[Instr].output)
+                MIPScode.append('addi '+reg1+',$0,'+Instr3AC[Instr].input1)
+            else:
+                reg1 = getreg(Instr, Instr3AC[Instr].input1)
+                if(symtables[Instr-initial+1][Instr3AC[Instr].input1][0] == DEAD):
+                    RegDescriptor[Instr3AC[Instr].output] = reg1
 
 #CODE GENERATION ALGORITHM
-def code_gen(initial,final):
-    for i in range(initial,final+1):
-        if Instr3AC[i].instrType in ['Arithmetic']:
-            x = Instr3AC[i].output
-            y = Instr3AC[i].input1
-            L = getreg(i,i-initial+1)
-
-            if (RegDescriptor[L] != y):
-                if ('register' in AddDescriptor[y]):
-                    for Regno in range(NumRegister):
-                        if (y == RegDescriptor[Regno]):
-                            break
-                    Y = Regno
-                    print 'move $t%d, $t%d',%(L,Y)
-                else:
-                    Y = y
-                    print 'lw $t%d, %s($zero)',%(L,Y)
-            z = Instr3AC[i].input2
-            op = giveOperator(Instr3AC[i].operator)
-            if(op == 0):
-                raise Exception("operator not correct")
-            if ('register' in AddDescriptor[z]):
-                for Regno in range(NumRegister):
-                    if (z == RegDescriptor[Regno]):
-                        break
-                Z = Regno
-                print '%s $t%d $t%d',%(op,L,Z)
-            else:
-                Z = z
-                print 'lw $t%d, '
-                print '%s t%d %s',%(op,L,Z)
-            RegDescriptor[L] = x
-            AddDescriptor[x].append('register')
-            for Regno in range(NumRegister):
-                if(Regno != L) and RegDescriptor[Regno] == x:
-                    RegDescriptor[Regno] = ''
-            symTableNo = i-initial+1
-            if (symtables[symTableNo][y][2] == 0):
-                for Regno in range(NumRegister):
-                    if(RegDescriptor[Regno]==y):
-                        RegDescriptor[Regno] = ''
-            if (symtables[symTableNo][z][2] == 0):
-                for Regno in range(NumRegister):
-                    if(RegDescriptor[Regno]==z):
-                        RegDescriptor[Regno] = ''
-
-        elif Instr3AC[i].instrType in ['Assignment']:
-        	x = Instr3AC[i].output
-            y = Instr3AC[i].input1
-            L = getreg(i,i-initial+1)
-            if(y.isdigit()==False):
-
-
-       
+# def code_gen(initial,final):
+#     for i in range(initial,final+1):
+#         if Instr3AC[i].instrType in ['Arithmetic']:
+#             x = Instr3AC[i].output
+#             y = Instr3AC[i].input1
+#             L = getreg(i,i-initial+1)
+#
+#             if (RegDescriptor[L] != y):
+#                 if ('register' in AddDescriptor[y]):
+#                     for Regno in range(NumRegister):
+#                         if (y == RegDescriptor[Regno]):
+#                             break
+#                     Y = Regno
+#                     print 'move $t%d, $t%d',%(L,Y)
+#                 else:
+#                     Y = y
+#                     print 'lw $t%d, %s($zero)',%(L,Y)
+#             z = Instr3AC[i].input2
+#             op = giveOperator(Instr3AC[i].operator)
+#             if(op == 0):
+#                 raise Exception("operator not correct")
+#             if ('register' in AddDescriptor[z]):
+#                 for Regno in range(NumRegister):
+#                     if (z == RegDescriptor[Regno]):
+#                         break
+#                 Z = Regno
+#                 print '%s $t%d $t%d',%(op,L,Z)
+#             else:
+#                 Z = z
+#                 print 'lw $t%d, '
+#                 print '%s t%d %s',%(op,L,Z)
+#             RegDescriptor[L] = x
+#             AddDescriptor[x].append('register')
+#             for Regno in range(NumRegister):
+#                 if(Regno != L) and RegDescriptor[Regno] == x:
+#                     RegDescriptor[Regno] = ''
+#             symTableNo = i-initial+1
+#             if (symtables[symTableNo][y][2] == 0):
+#                 for Regno in range(NumRegister):
+#                     if(RegDescriptor[Regno]==y):
+#                         RegDescriptor[Regno] = ''
+#             if (symtables[symTableNo][z][2] == 0):
+#                 for Regno in range(NumRegister):
+#                     if(RegDescriptor[Regno]==z):
+#                         RegDescriptor[Regno] = ''
+#
+#         elif Instr3AC[i].instrType in ['Assignment']:
+#         	x = Instr3AC[i].output
+#             y = Instr3AC[i].input1
+#             L = getreg(i,i-initial+1)
+#             if(y.isdigit()==False):
--- asgn2/three.py
+++ asgn2/three.py
@@ -1,11 +1,9 @@
-
 f = open('input.txt','r')
-
+MIPScode = []
 Instr3AC = []
 i=0
 Dead = 0
-Live = 1 
-labelIter = 1
+Live = 1
 class threeAddCode:
 	instrType = ""
 	input1 = ""
@@ -16,7 +14,6 @@ class threeAddCode:
 	lineNo = 0
 	leader = False
 	flabel = ""
-	labelName = ""
 
 def Equal(i,words):
 	Instr3AC[i].instrType = "Assignment"
@@ -38,8 +35,6 @@ def Ifgoto(i,words):
 	else:
 		Instr3AC[i].input1 = words[2]
 	Instr3AC[i].target = words[-1]
-	Instr3AC[words[-1]].labelName = "L"+str(labelIter)
-	labelIter = labelIter + 1 
 def Function(i,words):
 	Instr3AC[i].instrType = "FunctionCall"
 	Instr3AC[i].flabel = words[2]
@@ -65,11 +60,11 @@ for line in f:
 	if words[1] == '=':
 		Equal(i,words)
 	elif words[1] in ['+','-','/','*','%']:
-		Arithmetic(i,words)	
+		Arithmetic(i,words)
 	elif words[1] == 'ifgoto':
 		leaders.append(Instr3AC[i].lineNo + 1)
 		leaders.append(int(words[-1]))
-		Ifgoto(i,words)		
+		Ifgoto(i,words)
 	elif words[1] == 'call':
 		leaders.append(Instr3AC[i].lineNo + 1)
 		Function(i, words)
@@ -86,22 +81,38 @@ leaders.append(i)
 leaders = sorted(set(leaders))
 NumInstr = i
 # Building blockLength number of symbol tables (one for each program point)
+
+variables = []
+for i in range(NumInstr):
+	if (Instr3AC[i].input1.isdigit() == False and Instr3AC[i].input1!= ""):
+		variables.append(Instr3AC[i].input1)
+	if (Instr3AC[i].input2.isdigit() == False and Instr3AC[i].input2!= ""):
+		variables.append(Instr3AC[i].input2)
+	if (Instr3AC[i].output.isdigit() == False and Instr3AC[i].output!= ""):
+		variables.append(Instr3AC[i].output)
+
+variables = set(variables)
+MIPScode.append('.data')
+for var in variables:
+	MIPScode.append(var+': .word 0')
+print MIPScode
 for blockNum in range(len(leaders)-1):
-	initial = leaders[blockNum]	
+	initial = leaders[blockNum]
 	final = leaders[blockNum+1] - 1
 	blockLength = final - initial + 1
-	symtables = [dict() for x in range(blockLength+1)]
+	symtables = [dict() for x in range(blockLength)]
 	for blockLine in range(initial,final+1):
+
 		if (Instr3AC[blockLine].input1.isdigit() == False and Instr3AC[blockLine].input1!= ""):
 			if Instr3AC[blockLine].input1 not in symtables[0]:
-				for k in range(blockLength+1):
+				for k in range(blockLength):
 					symtables[k][Instr3AC[blockLine].input1] = [Dead, 0]
 		if (Instr3AC[blockLine].input2.isdigit() == False and Instr3AC[blockLine].input2!= ""):
 			if Instr3AC[blockLine].input2 not in symtables[0]:
-				for k in range(blockLength +1):
+				for k in range(blockLength):
 					symtables[k][Instr3AC[blockLine].input2] = [Dead, 0]
 		if Instr3AC[blockLine].output not in symtables[0] and Instr3AC[blockLine].output!="":
-				for k in range(blockLength+1):
+				for k in range(blockLength):
 					symtables[k][Instr3AC[blockLine].output] = [Dead, 0]
 	# print 'SYMBOL TABLE: '
 	# print symtables
@@ -109,14 +120,20 @@ for blockNum in range(len(leaders)-1):
 	scan = blockLength-1
 	for blockLine in range(final, initial -1 , -1):
 		if blockLine!=final:
-			symtables[scan]=symtables[scan+1].copy()								#copying the previous filled dictionary for further changes 
+			symtables[scan]=symtables[scan+1].copy()								#copying the previous filled dictionary for further changes
 		if (Instr3AC[blockLine].input1.isdigit() == False and Instr3AC[blockLine].input1!= ""):
 			symtables[scan][Instr3AC[blockLine].input1] = [Live, blockLine]
 		if (Instr3AC[blockLine].input2.isdigit() == False and Instr3AC[blockLine].input2!= ""):
 			symtables[scan][Instr3AC[blockLine].input2] = [Live, blockLine]
 		if (Instr3AC[blockLine].output!= ""):
-			symtables[scan][Instr3AC[blockLine].output] = [Dead, 0]			
+			symtables[scan][Instr3AC[blockLine].output] = [Dead, 0]
 		scan = scan-1
 		print 'symbol table before ' + str(blockLine)
 		print symtables[blockLine-initial]
+
+	initializeReg()
+	code_gen(initial,final)
+	dumpReg()
+
+
 #	print symtables
