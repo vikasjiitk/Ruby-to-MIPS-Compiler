349626ef890d998e2a75935668ceee99a092169a
Vikas Jain <vikasj@cse.iitk.ac.in>
2016-02-07 03:18:38 +0530

    one test file

--- asgn2/test/test8.s
+++ asgn2/test/test8.s
@@ -1,6 +1,5 @@
 .data
 a: .word 0
-leq: .word 0
 c: .word 0
 belikejanish: .word 0
 .text
@@ -13,25 +12,33 @@ addi $t2,$0,1
 L3: 
 lw $t1,a
 add $t1,$t1,$t2
-sw $t1,a
 sw $t2,belikejanish
 L4: 
-lw $t2,leq
-bgtz $t2,L4
-sw $t2,leq
+sw $t1,a
+lw $t1,a
+addi $t2,$0,5
+ble $t1,$t2,L2
+sw $t1,a
 L5: 
 jal foo
 move $t2,$v1
 sw $t2,c
-foo: 
+L6: 
+li $v0, 1
+lw $t2,c
+move $a0, $t2
+syscall
 L7: 
+li $v0, 10
+syscall
+sw $t2,c
+foo: 
+L9: 
 li $v0, 1
 lw $t2,a
 move $a0, $t2
 syscall
-L8: 
-li $v0, 10
-syscall
-L9: 
+L10: 
+move $v1, $t2
 jr $ra
 sw $t2,a
