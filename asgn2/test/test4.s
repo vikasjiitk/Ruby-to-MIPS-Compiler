a5d98bc53e735b4be77397e3f4e81cfdaab316e9
Vikas Jain <vikasj@cse.iitk.ac.in>
2016-02-07 03:12:06 +0530

    one test file

--- asgn2/test/test4.s
+++ asgn2/test/test4.s
@@ -6,15 +6,17 @@ L1:
 jal foo
 move $t2,$v1
 sw $t2,a
-foo: 
-L3: 
-li $v0, 10
-syscall
-L4: 
+L2: 
 li $v0, 1
 lw $t2,a
 move $a0, $t2
 syscall
-L5: 
-jr $ra
+L3: 
+li $v0, 10
+syscall
 sw $t2,a
+foo: 
+L5: 
+addi $t2,$0,2
+move $v1, $t2
+jr $ra
