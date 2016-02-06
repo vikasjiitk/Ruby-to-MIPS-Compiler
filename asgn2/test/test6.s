0c9d68dd6a2abd76ac47b6d3e45a62581e59f9f9
Vikas Jain <vikasj@cse.iitk.ac.in>
2016-02-07 01:35:16 +0530

    deleting extra files

--- asgn2/test/test6.s
+++ asgn2/test/test6.s
@@ -4,35 +4,36 @@ c: .word 0
 d: .word 0
 .text
 main: 
-L0: 
+main: 
+L1: 
 lw $s7,a
 addi $s7,$0,2
 sw $s7,a
-L1: 
+L2: 
 lw $s7,d
 addi $s7,$0,1
+L3: 
+lw $s6,a
+add $s6,$s6,$s7
 sw $s7,d
-L2: 
-jal b
+L4: 
+sw $s6,a
+lw $s6,a
+addi $s7,$0,5
+ble $s6,$s7,L2
+sw $s6,a
+L5: 
+jal foo
 lw $s7,c
 move $s7,$v1
 sw $s7,c
-li $v0,10
-syscall
-b: 
-L4: 
-lw $s7,a
-lw $s6,d
-add $s7,$s7,$s6
-sw $s6,d
-L5: 
-move $v1, $s7
-jr $ra
-sw $s7,a
-L6: 
+foo: 
+L7: 
 li $v0, 1
-lw $s7,c
+lw $s7,a
 move $a0, $s7
 syscall
-sw $s7,c
-
+L8: 
+li $v0, 10
+syscall
+sw $s7,a
